3f8215c9488852ba1b2d52b419614cf1e0707349
Rafael <rafael.fcardoso@hotmail.com>
2022-02-03 17:38:54 -0300

    Cria e renderiza o componente Album

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom/cjs/react-router-dom.min';
 import Login from './pages/Login';
 import Search from './pages/Search';
+import Album from './pages/Album';
 
 class App extends React.Component {
   render() {
@@ -11,6 +12,7 @@ class App extends React.Component {
         <BrowserRouter>
           <Route exact path="/" component={ Login } />
           <Route exact path="/search" component={ Search } />
+          <Route exact path="/album/:id" component={ Album } />
         </BrowserRouter>
       </div>);
   }
