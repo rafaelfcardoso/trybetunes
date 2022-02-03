90d63348c6790cbb125c0a0150ce66fe609e88cc
Rafael <rafael.fcardoso@hotmail.com>
2022-02-03 17:43:17 -0300

    Cria e renderiza o componente Favorites

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route } from 'react-router-dom/cjs/react-router-dom.min'
 import Login from './pages/Login';
 import Search from './pages/Search';
 import Album from './pages/Album';
+import Favorites from './pages/Favorites';
 
 class App extends React.Component {
   render() {
@@ -13,6 +14,7 @@ class App extends React.Component {
           <Route exact path="/" component={ Login } />
           <Route exact path="/search" component={ Search } />
           <Route exact path="/album/:id" component={ Album } />
+          <Route exact path="/favorites" component={ Favorites } />
         </BrowserRouter>
       </div>);
   }
