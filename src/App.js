5914cc2cdc598187869300540c2efc6bbb88ee7d
Rafael <rafael.fcardoso@hotmail.com>
2022-02-03 17:16:54 -0300

    Cria e renderiza componente Login.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom/cjs/react-router-dom.min';
+import Login from './pages/Login';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <div>
+        <p>TrybeTunes</p>
+        <BrowserRouter>
+          <Route exact path="/" component={ Login } />
+        </BrowserRouter>
+      </div>);
   }
 }
 
