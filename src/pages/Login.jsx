a5adb0ca02fd245d6df8a91e8622a5fbec6145d8
Rafael <rafael.fcardoso@hotmail.com>
2022-02-04 14:38:36 -0300

    Implementa o formulario Login com input Submit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import { React, useState } from 'react';
+// import ReactDOM from 'react-dom';
 
-class Login extends React.Component {
-  render() {
-    return (
-      <div data-testid="page-login">Login</div>
-    );
-  }
+function Login() {
+  const [name, setName] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <form onSubmit={ handleSubmit } data-testid="page-login">
+      <label htmlFor="login-name-input">
+        Enter your name:
+        <input
+          type="text"
+          data-testid="login-name-input"
+          value={ name }
+          onChange={ (e) => setName(e.target.value) }
+        />
+      </label>
+      <input type="Submit" data-testid="login-submit-button" />
+    </form>
+  );
 }
 
+// ReactDOM.render(<Login />, document.getElementById('root'));
 export default Login;
