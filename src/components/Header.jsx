94a918d1ee6cf35670da3c3f15cffde187cafd03
Rafael <rafael.fcardoso@hotmail.com>
2022-02-06 17:28:42 -0300

    Chama a funcao getUser, rederiza o componente e a mensagem 'Carregando...'

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
-import Mensagem from '../pages/Mensagem';
+import Loading from '../pages/Loading';
 import { getUser } from '../services/userAPI';
 
 class Header extends Component {
   state = {
     loading: false,
-    profileName: '',
+    userName: '',
+  };
+
+  componentDidMount() {
+    this.getInfoUser();
+  }
+
+  getInfoUser = async () => {
+    this.setState({ loading: true });
+    const { userName } = await getUser();
+    this.setState({ userName, loading: false });
   };
 
   render() {
-    const { loading, profileName } = this.state;
+    const { loading, userName } = this.state;
     return (
       <header data-testid="header-component">
+        {loading ? <Loading /> : <p data-testid="header-user-name">{userName}</p>}
       </header>
     );
   }
