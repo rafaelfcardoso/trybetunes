3023649dc04dc9912cc98b19bfa20f1c5c06ecdd
Rafael <rafael.fcardoso@hotmail.com>
2022-02-06 22:22:21 -0300

    Cria um campo para pessoa digitar o nome da banda ou artista a ser pesquisada.

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,29 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  state = {
+    artistName: '',
+  };
+
+  handleChange = ({ target: { value } }) => {
+    this.setState({ artistName: value }, () => this.handleButtonStatus());
+  };
+
   render() {
+    const { artistName } = this.state;
+
     return (
       <main>
         <Header />
-        <div data-testid="page-search">Search</div>
+        <div data-testid="page-search">
+          <input
+            data-testid="search-artist-input"
+            type="text"
+            value={ artistName }
+            onChange={ this.handleChange }
+          />
+          Pesquisar
+        </div>
       </main>
     );
   }
