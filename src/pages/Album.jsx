7ff3341ed4249c1f4010814a1db424b580e94af6
Rafael <rafael.fcardoso@hotmail.com>
2022-02-07 16:52:45 -0300

    Requisito 07, implementa func requestMusic, renderiza-a no componente Album

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,15 +1,54 @@
 import React from 'react';
 import Header from '../components/Header';
+import getMusics from '../services/musicsAPI';
 
 class Album extends React.Component {
+  state = {
+    artistName: '',
+    collectionName: '',
+    artworkUrl100: '',
+    songs: [],
+  };
+
+  componentDidMount() {
+    this.requestMusic();
+  }
+
+  requestMusic = async () => {
+    const { id } = this.props;
+    const responseAPI = await getMusics(id);
+    const { artistName, collectionName, artworkUrl100 } = responseAPI[0];
+    this.setState({
+      artistName,
+      collectionName,
+      artworkUrl100,
+      songs: responseAPI.slice(1),
+    });
+  };
+
   render() {
     return (
       <main>
         <Header />
-        <div data-testid="page-album">Album</div>
+        <div data-testid="page-album">
+          <p data-testid="artist-name">{artistName}</p>
+          <p data-testid="album-name">{collectionName}</p>
+          <img src={ artworkUrl100 } alt={ collectionName } />
+
+          {songs.map(({ songName, songId, previewUrl }) => (
+            <div key={ songId }>
+              <span>{songName}</span>
+              <MusicCard url={ previewUrl } />
+            </div>
+          ))}
+        </div>
       </main>
     );
   }
 }
+
+Album.propTypes = {
+  id: PropTypes.string,
+}.isRequired;
 
 export default Album;
