37c9ae069f2e23b47c0f175704fb8e20b6907c28
Rafael <rafael.fcardoso@hotmail.com>
2022-06-12 16:05:38 -0300

    feat: run trybe-filter-repo.sh

---
+++ src/components/Header/styles.js
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+
+export const Header = styled.div`
+  background-color: ${(props) => props.theme.colors.primary.background};
+  position: absolute;
+  width: 100%;
+  height: 100px;
+  left: 0px;
+  top: 0px;
+  display: flex;
+  justify-content: space-between;
+
+  a {
+    color: white;
+    width: 87px;
+    height: 22px;
+    margin-right: 30px;
+    margin-left: 30px;
+    align-self: center;
+
+    font-style: normal;
+    font-weight: 600;
+    font-size: 18px;
+    line-height: 22px;
+  }
+`;
+
+export const Container = styled.div`
+  display: flex;
+`;
+
+export const User = styled.p`
+  color: white;
+  width: 87px;
+  text-align: right;
+  height: 22px;
+  margin-right: 30px;
+  align-self: center;
+
+  font-style: normal;
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 22px;
+`;
